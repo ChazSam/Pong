0f6e51802a46dd5cf6441c9f3360ad6b201f67e5
ChazSam <129200361+ChazSam@users.noreply.github.com>
2025-11-24 18:37:56 -0600

    added barriers and goals

--- Scene/paddle.gd
+++ Scene/paddle.gd
@@ -4,5 +4,5 @@ extends CharacterBody2D
 @export var movement_speed: float = 900
 @onready var screen_size = get_viewport_rect().size
 
-func _process(delta: float) -> void:
-		position.y = clamp(position.y, 5, screen_size.y - $ColorRect.size.y + 5)
+func _process(_delta: float) -> void:
+		position.y = clamp(position.y, 5, screen_size.y - $ColorRect.size.y - 5)
--- Scene/player_1.gd
+++ Scene/player_1.gd
@@ -6,11 +6,9 @@ func get_input():
 	velocity.y = input_vector * movement_speed
 
 func _physics_process(_delta: float) -> void:
-	position.y = clamp(position.y, 0, screen_size.y - $ColorRect.size.y)
+	#position.y = clamp(position.y, 0, screen_size.y - $ColorRect.size.y)
 	
-	#var sprite = $ColorRect
-	#var size = sprite.size.y
-	#print(size)
+
 	
 	get_input()
 	move_and_slide()
